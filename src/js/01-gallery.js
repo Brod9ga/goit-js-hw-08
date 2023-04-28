15885524377418738b664929f5802397402700b0
Brod9ga <gbonik@i.ua>
2023-04-28 23:20:34 +0300

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from './gallery-items';
 
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
+import  "simplelightbox/dist/simple-lightbox.min.css";
 
 // Change code below this line
 
